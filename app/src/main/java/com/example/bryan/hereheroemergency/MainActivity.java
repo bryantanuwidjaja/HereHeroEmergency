f58f823f9a18239652415aec9025502b3245c2e1
Bryan <bryantanuwijaya12@gmail.com>
2018-09-29 19:06:09 +0800

    Adding Panic Button on Home Activity
    Panic Button has 2 Stage, additional on disabled button but still no function to do with the disabled button

--- app/src/main/java/com/example/bryan/hereheroemergency/MainActivity.java
+++ app/src/main/java/com/example/bryan/hereheroemergency/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.bryan.hereheroemergency;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button panicButton = findViewById(R.id.panic_button);
+
+        panicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Click",Toast.LENGTH_SHORT).show();;
+            }
+        });
     }
 }
