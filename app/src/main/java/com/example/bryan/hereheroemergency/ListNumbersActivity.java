ef9f0ccdeeb0a8cb6af78357c6eff403b8d72ab7
Bryan <bryantanuwijaya12@gmail.com>
2018-10-20 19:14:54 +0800

    add strings

--- app/src/main/java/com/example/bryan/hereheroemergency/ListNumbersActivity.java
+++ app/src/main/java/com/example/bryan/hereheroemergency/ListNumbersActivity.java
@@ -9,10 +9,31 @@ import android.support.design.widget.TabLayout;
 
 public class ListNumbersActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+
+        ListNumber number1 = new ListNumber ("USA","911","911","911",R.drawable.ic_person);
+        arrayListNumbers.add(number1);
+        ListNumber number2 = new ListNumber ("Canada","911","911","911",R.drawable.ic_person);
+        arrayListNumbers.add(number2);
+        ListNumber number3 = new ListNumber ("Mexico","066","066","066",R.drawable.ic_person);
+        arrayListNumbers.add(number3);
+        ListNumber number4 = new ListNumber ("China","110","120","119",R.drawable.ic_person);
+        arrayListNumbers.add(number4);
+        ListNumber number5 = new ListNumber ("India","100","102","101",R.drawable.ic_person);
+        arrayListNumbers.add(number5);
+        ListNumber number6 = new ListNumber ("Indonesia","110","118","113",R.drawable.ic_person);
+        arrayListNumbers.add(number6);
+        ListNumber number7 = new ListNumber ("Iran","110","115","125",R.drawable.ic_person);
+        arrayListNumbers.add(number7);
+        ListNumber number8 = new ListNumber ("Malaysia","999","999","999",R.drawable.ic_person);
+        arrayListNumbers.add(number8);
+        ListNumber number9 = new ListNumber ("Singapore","999","995","995",R.drawable.ic_person);
+        arrayListNumbers.add(number9);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
