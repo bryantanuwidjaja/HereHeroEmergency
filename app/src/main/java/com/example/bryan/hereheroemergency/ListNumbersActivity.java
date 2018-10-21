beac955f404753ccfcab4287b1c2f6fdbfaa312a
Bryan <bryantanuwijaya12@gmail.com>
2018-10-21 11:53:17 +0800

    small tidy up

--- app/src/main/java/com/example/bryan/hereheroemergency/ListNumbersActivity.java
+++ app/src/main/java/com/example/bryan/hereheroemergency/ListNumbersActivity.java
@@ -10,33 +10,13 @@ import java.util.ArrayList;
 
 
 public class ListNumbersActivity extends AppCompatActivity {
-    private ArrayList<ListNumbers> arrayListNumbers = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listnumbers);
 
-
-
-        ListNumbers number1 = new ListNumbers ("USA","911","911","911",R.drawable.usa);
-        arrayListNumbers.add(number1);
-        ListNumbers number2 = new ListNumbers ("Canada","911","911","911",R.drawable.canada);
-        arrayListNumbers.add(number2);
-        ListNumbers number3 = new ListNumbers ("Mexico","066","066","066",R.drawable.mexico);
-        arrayListNumbers.add(number3);
-        ListNumbers number4 = new ListNumbers ("China","110","120","119",R.drawable.china);
-        arrayListNumbers.add(number4);
-        ListNumbers number5 = new ListNumbers ("India","100","102","101",R.drawable.india);
-        arrayListNumbers.add(number5);
-        ListNumbers number6 = new ListNumbers ("Indonesia","110","118","113",R.drawable.indonesia);
-        arrayListNumbers.add(number6);
-        ListNumbers number7 = new ListNumbers("Iran","110","115","125",R.drawable.iran);
-        arrayListNumbers.add(number7);
-        ListNumbers number8 = new ListNumbers ("Malaysia","999","999","999",R.drawable.malaysia);
-        arrayListNumbers.add(number8);
-        ListNumbers number9 = new ListNumbers ("Singapore","999","995","995",R.drawable.singapore);
-        arrayListNumbers.add(number9);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
